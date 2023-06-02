399dbe21896209200137f95b8fa993c6bca0fced
PedroL <pedro.lopesverardo@gmail.com>
2023-06-02 11:21:29 -0300

    mudanças na logica do save

--- src/Model/Save.java
+++ src/Model/Save.java
@@ -1,9 +1,31 @@
 package Model;
 
 import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 class Save {
-	void WriteF(String name) {
+	FileWriter createF(String name) throws IOException {
 		FileWriter writeNewFile = new FileWriter(name + ".txt");
+		
+		return writeNewFile;
+	}
+	
+	void writeIntegerList(ArrayList<List<Integer>> listAll) {
+		
+	}
+	
+	
+	void writePawnsState(String name, Facade f) {
+		try {
+			FileWriter newF = createF(name);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		ArrayList<List<Integer>> listAll = new ArrayList<List<Integer>>();
 	}
 }
