dba4d1803d923243b065ad3e4736836e3cb9f020
PedroL <pedro.lopesverardo@gmail.com>
2023-06-09 19:38:23 -0300

    get color

--- src/Model/Facade.java
+++ src/Model/Facade.java
@@ -120,8 +120,8 @@ public class Facade {
 		return lis.get(local).getColor();
 	}
 	
-	public int getPlayerColor() {
-		return acctualPlayer.getPlayerColor().getValue();
+	public PlayerColor getPlayerColor() {
+		return acctualPlayer.getPlayerColor();
 	}
 	
 	public void saveGame(){
